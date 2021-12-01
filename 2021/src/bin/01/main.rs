8da404da65f3deb47afa40023cf7153f4d7f6fdb
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-12-01 17:59:58 +0100

    Solution day 1

--- 2021/src/bin/01/main.rs
+++ 2021/src/bin/01/main.rs
@@ -1,11 +1,64 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+};
+
+fn number_of_increases(measurements: Vec<u16>) -> u16 {
+    let mut count = 0;
+    for i in 1..measurements.len() {
+        if measurements[i - 1] < measurements[i] {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn number_of_increases_sliding_window(measurements: Vec<u16>) -> u16 {
+    let mut count = 0;
+
+    for i in 1..measurements.len() - 2 {
+        let current_sum = measurements[i - 1] + measurements[i] + measurements[i + 1];
+        let next_sum = measurements[i] + measurements[i + 1] + measurements[i + 2];
+
+        if current_sum < next_sum {
+            count += 1;
+        }
+    }
+    count
+}
+
 fn main() {
-    println!("Hello, Advent of Code!");
+    // read file to vector
+    let file = File::open("input").expect("file not found");
+    let buf = BufReader::new(file);
+
+    let numbers: Vec<u16> = buf
+        .lines()
+        .map(|line| line.unwrap().parse::<u16>().unwrap())
+        .collect();
+
+    println!(
+        "Number of increases: {}",
+        number_of_increases(numbers.clone())
+    );
+    println!(
+        "Number of increases sliding window: {}",
+        number_of_increases_sliding_window(numbers)
+    );
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_number_of_increases() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(number_of_increases(input), 7);
+    }
+
+    #[test]
+    fn test_number_of_increases_sliding_window() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(number_of_increases_sliding_window(input), 5);
     }
 }
