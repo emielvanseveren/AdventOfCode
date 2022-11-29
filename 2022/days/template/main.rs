d652f55400b1e2aa7dc4708c54d39e678f93b01d
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-11-29 18:01:28 +0100

    Setup repo for aoc 2022

---
+++ 2022/days/template/main.rs
@@ -0,0 +1,22 @@
+
+fn part_1(){
+    unimplemented!()
+}
+
+fn part_2() {
+    unimplemented!()
+}
+
+fn main() {
+    println!("Hello, Advent of Code!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ 2022/readme.md
@@ -0,0 +1,5 @@
+Run code of day:
+
+```
+cargo run --bin 01
+```
