3387b45e3efaf0b0db22569ca06a81a72db3be4b
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-12-02 23:48:51 +0100

    Add setup 2023

---
+++ 2023/days/template/main.rs
@@ -0,0 +1,26 @@
+fn part_1() {
+    unimplemented!();
+}
+
+fn part_2() {
+    unimplemented!();
+}
+
+fn main() {
+    println!("Hello, Advent of Code!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_example() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn part_2_example() {
+        unimplemented!();
+    }
+}
