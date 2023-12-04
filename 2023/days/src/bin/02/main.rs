3057961b8a6116a891f07b24e96336b28819a0f9
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-12-04 13:23:54 +0100

    Fix: use const

--- 2023/days/src/bin/02/main.rs
+++ 2023/days/src/bin/02/main.rs
@@ -74,7 +74,7 @@ fn main() {
 mod tests {
     use super::*;
 
-    static input: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
@@ -82,11 +82,11 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
     #[test]
     fn part_1_example() {
-        assert_eq!(part_1(input), 8);
+        assert_eq!(part_1(INPUT), 8);
     }
 
     #[test]
     fn part_2_example() {
-        assert_eq!(part_2(input), 2286);
+        assert_eq!(part_2(INPUT), 2286);
     }
 }
